d0d9b1c31798c7679fd7499c5dc486b78cdf8a51
Rongyao <cn.royalchan@gmail.com>
2024-05-31 15:13:03 +0800

    Update git-aliases.sh

--- git-aliases.sh
+++ git-aliases.sh
@@ -2,5 +2,3 @@ echo `
 gcr(){git fetch origin $1 && git checkout -b $1 origin/${1};}
 export -f gcr
 ` >> ~/.zshrc
-
-echo "alias gcr='gcr_func(){git fetch origin $1 && git checkout -b $1 origin/$1;};gcr_func'" >> ~/.zshrc
